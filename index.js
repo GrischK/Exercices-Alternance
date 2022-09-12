fca2c8264e59e79a1e86e804f776231c0f4bfa28
GrischK <grischka.gorski@gmail.com>
2022-09-12 14:53:32 +0200

    first_save

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('salut')
+const express = require("express");
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+  console.log("test");
+  res.send("hello")
+});
+
+app.listen(3000, () => {
+  console.log("salut");
+});
